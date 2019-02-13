ce4b88fb55e89d6f9b3098fb07f89b64eb67ce8b
Marcin Pogorzelski <mpogorzelski@infoprojekt.pl>
2019-02-13 12:09:36 +0100

    added project

---
+++ CSharpBasic/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    class Program
+    {
+
+       
+        static void Main(string[] args)
+        {
+
+            TestClass1 testClass = new TestClass1();
+            testClass.Add(12, 12);
+            testClass.Add();
+           // NewMethod();
+            Console.Read();
+
+        }
+
+
+
+
+        private static void NewMethod()
+        {
+            TestClass2 class1 = new TestClass2();
+            class1.TestCase2("Passed");
+            class1.TestCase2("Failed");
+
+
+            class1.SetValue(20);
+            class1.GetValue();
+
+
+            TestClass2 class2 = new TestClass2();
+            class2.SetValue(30);
+            class2.GetValue();
+
+            Console.Write("Hello Pogo");
+
+
+
+        }
+    }
+}
---
+++ CSharpBasic/TestClass1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasic
+{
+    class TestClass1
+    {
+       public void Add()
+        {
+            Console.WriteLine("Empty method");
+        }
+        public void Add(int num1,int num2)
+        {
+            int result = num1 + num2;
+            Console.WriteLine("Empty method with two parameters " + result);
+        }
+    }
+
+    class TestClass2
+    {
+        private int i = 0;
+
+        public void SetValue(int value)
+        {
+            i = value;
+        }
+
+        public void GetValue()
+        {
+            Console.WriteLine("The Value of i is: " + i);
+        }
+
+        public void TestCase1(string result)
+        {
+            Console.WriteLine("Executing TEstCase one " + result);
+        }
+
+        public void TestCase2(string result)
+        {
+            Console.WriteLine("Executing TEstCase two " + result);
+        }
+
+
+    }
+
+}
